d8cf6829425f2f6f5391708a9be2145d249e3dbd
Veen <d.veen@uu.nl>
2018-11-26 13:14:41 +0100

    Initial commit

---
+++ R/modelcompilation-package.R
@@ -0,0 +1,17 @@
+#' The 'modelcompilation' package.
+#' 
+#' @description A DESCRIPTION OF THE PACKAGE
+#' 
+#' @docType package
+#' @name modelcompilation-package
+#' @aliases modelcompilation
+#' @useDynLib modelcompilation, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @import rstantools
+#' @importFrom rstan sampling
+#' 
+#' @references 
+#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.18.2. http://mc-stan.org
+#' 
+NULL
